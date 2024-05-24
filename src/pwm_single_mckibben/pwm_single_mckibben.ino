af3ca2d802b123c2eee24f031b1da8104c6d2a32
Melara <105904994+Melara1024@users.noreply.github.com>
2024-05-24 22:10:10 +0900

    Create pwm_single_mckibben.ino

---
+++ src/pwm_single_mckibben/pwm_single_mckibben.ino
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+#include <math.h>
+
+unsigned long startmillis = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  //このプログラムは基本的にUNO向けなので注意
+  DDRD = DDRD | B11111100;
+  DDRB = DDRB | B00000011;
+
+  //関節ポテンショ
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+
+  //圧力センサ
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+
+  startmillis = millis();
+}
+
+int target = 600;
+int pressure = 0;
+
+unsigned long stopwatch = 0;
+
+int opentime = 0;
+
+
+void loop() {
+  stopwatch = millis();
+  
+  pressure = analogRead(A5);
+
+  if (millis() > (startmillis + 1000UL)) {
+    if (target == 600) target = 700;
+    else target = 600;
+    startmillis = millis();
+  }
+
+  Serial.print(pressure);
+  Serial.print(",");
+  Serial.println(target);
+  
+  if (pressure > target) {
+    digitalWrite(8, 1);
+    digitalWrite(9, 0);
+    
+  } 
+  else if (pressure < target){
+    digitalWrite(8, 0);
+    digitalWrite(9, 1);
+    
+  }
+
+  opentime = constrain(abs(target - pressure)*0.2, 0, 29);
+  delay(opentime);
+  
+  digitalWrite(8, 0);
+  digitalWrite(9, 0);
+
+  delay((stopwatch + 30) - millis());
+}
